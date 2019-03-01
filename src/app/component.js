819f25b4706ad59e2d3bb9b152c1f6106d30df57
8RockStar8 <gagik.aproyan.dev@gmail.com>
2019-03-01 21:32:23 +0400

    start create react app, remove default files, init app folder

---
+++ src/app/component.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+    render() {
+        return(
+            <div>return from app</div>
+        );
+    }
+}
+
+export default App;
