6ae3712523839274a2c44e52303e4e045c858857
8RockStar8 <gagik.aproyan.dev@gmail.com>
2019-03-01 21:41:02 +0400

    init header component

--- src/app/component.js
+++ src/app/component.js
@@ -1,9 +1,14 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 
+import Header from './header/component';
+
+import './css/style.css';
 class App extends PureComponent {
     render() {
         return(
-            <div>return from app</div>
+            <Fragment>
+                <Header />
+            </Fragment>
         );
     }
 }
---
+++ src/app/header/component.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import './css/style.css';
+
+class Header extends PureComponent {
+    render() {
+        return(
+            <header>header</header>
+        )
+    }
+}
+
+export default Header;
