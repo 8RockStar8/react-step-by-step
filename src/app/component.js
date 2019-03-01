000cfa33a1a184cc4fb0ff9e31025580ba90f29d
8RockStar8 <gagik.aproyan.dev@gmail.com>
2019-03-01 22:19:20 +0400

    init footer component

--- src/app/component.js
+++ src/app/component.js
@@ -1,6 +1,7 @@
 import React, { PureComponent, Fragment } from 'react';
 
 import Header from './header/component';
+import Footer from './footer/component';
 
 import './css/style.css';
 class App extends PureComponent {
@@ -8,6 +9,7 @@ class App extends PureComponent {
         return(
             <Fragment>
                 <Header />
+                <Footer />
             </Fragment>
         );
     }
---
+++ src/app/footer/component.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import './css/style.css';
+
+class Footer extends PureComponent {
+    render() {
+        return(
+            <footer>footer</footer>
+        )
+    }
+}
+
+export default Footer;
