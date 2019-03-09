1df5fce8ee8ccd0674631bc60773655f201bffda
8RockStar8 <gagik.aproyan.dev@gmail.com>
2019-03-09 19:43:23 +0400

    add empty line the end of the file, and change braces for currect code styleing

--- src/App/components/Content/component.js
+++ src/App/components/Content/component.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react';
 class Content extends PureComponent {
     render() {
         return (
-            <div className="content">
+            <div className='content'>
                 Content
             </div>
         );
