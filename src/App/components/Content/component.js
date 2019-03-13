3ab0a2abb14bafa63db37bd2de7a195ea4ce8b3f
8RockStar8 <gagik.aproyan.dev@gmail.com>
2019-03-13 21:32:54 +0400

    change users rendering logic, try work with material ui

--- src/App/components/Content/component.js
+++ src/App/components/Content/component.js
@@ -1,48 +1,18 @@
 import React, { PureComponent } from 'react';
 
+import Users from './Users';
+
 import './component.css';
 class Content extends PureComponent {
     constructor(props) {
         super(props);
-        this.state = {
-            items: []
-        };
-    }
-
-    componentDidMount() {
-        fetch('https://api.github.com/users')
-        .then(res => res.json())
-        .then(
-            (res) => {
-                this.setState({
-                    items: res
-                });
-            }
-        );
     }
 
     render() {
-        const { items } = this.state;
         return (
             <div className='content'>
                 <div className='content-center'>
-                    <div className='user-item-place'>
-                        {
-                            items.map(item => (
-                                <div className='user-item-block' key={item.id}>
-                                    <a href={item.html_url} target='_blank' rel='noopener noreferrer' className='user-link'>
-                                        <img src={item.avatar_url} className='user-item-img' alt='user' />
-                                    </a>
-                                    <div className='user-item-text-place'>
-                                        <p className='user-item-text'>
-                                            {item.login}
-                                        </p>
-                                    </div>
-                                </div>
-                                )
-                            )
-                        }
-                    </div>
+                    <Users />
                 </div>
             </div>
         );
