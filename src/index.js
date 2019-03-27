b565da41cfa6caa1a9daf5b9375d583972642d18
Gagik Aproyan <gagik@x-tech.am>
2019-03-27 17:35:44 +0400

    commit for fix merge duplicate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
-import App from './sections';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
