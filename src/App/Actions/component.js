31b24856286520f54ae0486acef3fd0ce4b00f76
8RockStar8 <gagik.aproyan.dev@gmail.com>
2019-04-05 00:00:25 +0400

    understand redux store dispatch, subscribe functions, use fetch for getting data from api

--- src/App/Actions/component.js
+++ src/App/Actions/component.js
@@ -2,27 +2,38 @@ import React from 'react';
 
 import { createStore } from 'redux';
 
-function getUsers(state = [], action) {
+const initState = {
+    users: []
+}
+
+function reducer(state = initState, action) {
     if (action.type === 'FETCH_GET') {
-        return fetch('https://api.github.com/users')
-            .then(res => res.json())
-            .then(
-                (res) => [
-                    ...state,
-                    res
-                ]
-            )
+        return {
+            ...state, 
+            users: [...state.users, action.payload]
+        };
     }
     return state;
 }
 
-const store = createStore(getUsers);
+const store = createStore(reducer);
+
+var newUsers = [];
 
 store.subscribe(() => {
-    console.log(store.getState());
+    newUsers = store.getState();
+    console.log(newUsers.users[0].length);
 });
 
-store.dispatch({ type: 'FETCH_GET' });
+
+fetch('https://api.github.com/users')
+        .then(res => res.json())
+        .then((res) => {
+            store.dispatch({ type: 'FETCH_GET', payload: res });
+        });
+
+
+
 
 function Action() {
     return (
